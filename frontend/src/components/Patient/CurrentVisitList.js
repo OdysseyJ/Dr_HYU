b58345583fb98077915491251ef345ca88354697
SeongWoon <odysseyj@naver.com>
2019-12-09 19:44:34 +0900

    path

---
+++ frontend/src/components/Patient/CurrentVisitList.js
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+import * as logAPI from "../../lib/api/log";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Typography from "@material-ui/core/Typography";
+
+//for redux.
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as userActions from "../../redux/modules/user";
+
+class CurrentVisitList extends Component {
+  state = {
+    logs: []
+  };
+
+  getPatientLog = async () => {
+    const { email } = this.props.loggedInfo.toJS();
+    const logs = await logAPI.getPatientLog({
+      uemail: email
+    });
+    this.setState({ logs: logs.data });
+  };
+
+  componentDidMount() {
+    this.getPatientLog();
+  }
+
+  render() {
+    console.log(this.state.logs);
+    return (
+      <div>
+        <Typography style={{ paddingLeft: 30, paddingTop: 50 }} variant="h5">
+          최근방문목록
+        </Typography>
+        <div
+          style={{
+            paddingLeft: 30,
+            paddingRight: 30
+          }}
+        >
+          <div>
+            <List>
+              {this.state.logs.length > 0 &&
+                this.state.logs.map(p => {
+                  if (p.logtype === "reservation") {
+                    let name;
+                    if (p.sname === null) {
+                      name = p.hname;
+                    } else if (p.hname === null) {
+                      name = p.sname;
+                    }
+                    const split = p.time.split(" ");
+                    const year = split[0];
+                    const month = split[1];
+                    const day = split[2];
+                    const time = split[3];
+                    const total = `예약시간 : ${year}년 ${month}월 ${day}일 ${time}시`;
+                    return (
+                      <ListItem key={p.id}>
+                        <ListItemText primary={name} secondary={total} />
+                      </ListItem>
+                    );
+                  } else return null;
+                })}
+            </List>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default connect(
+  state => ({
+    loggedInfo: state.user.get("loggedInfo"),
+    logged: state.user.get("logged")
+  }),
+  dispatch => ({
+    UserActions: bindActionCreators(userActions, dispatch)
+  })
+)(CurrentVisitList);
---
+++ frontend/src/components/Patient/FavoriteList.js
@@ -0,0 +1,139 @@
+import React, { Component } from "react";
+import * as favoriteAPI from "../../lib/api/favorite";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import DeleteIcon from "@material-ui/icons/Delete";
+
+//for redux.
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as userActions from "../../redux/modules/user";
+
+import { Button } from "../../components/Util";
+import { Dialog, DialogTitle, DialogActions } from "@material-ui/core";
+
+class FavoriteList extends Component {
+  state = {
+    favorites: [],
+    isPopupShow: false,
+    uemail: "",
+    sname: "",
+    hname: ""
+  };
+
+  getFavorites = async () => {
+    const { email } = this.props.loggedInfo.toJS();
+    const favorites = await favoriteAPI.getFavorites({
+      uemail: email
+    });
+    this.setState({ favorites: favorites.data });
+  };
+
+  handleDeleteButton = (data, e) => {
+    const { uemail, sname, hname } = data.p;
+    this.setState({
+      isPopupShow: true,
+      uemail: uemail,
+      sname: sname,
+      hname: hname
+    });
+  };
+
+  handleDeleteComplete = async () => {
+    const { uemail, hname, sname } = this.state;
+    await favoriteAPI.deleteFavorite({
+      uemail: uemail,
+      hname: hname,
+      sname: sname
+    });
+    const favorites = await favoriteAPI.getFavorites({
+      uemail: uemail,
+      hname: hname,
+      sname: sname
+    });
+    this.setState({
+      isPopupShow: false,
+      uemail: "",
+      hname: "",
+      sname: "",
+      favorites: favorites.data
+    });
+  };
+
+  handleExitButton = () => {
+    this.setState({ isPopupShow: false, selectedId: "" });
+  };
+
+  componentDidMount() {
+    this.getFavorites();
+  }
+
+  render() {
+    return (
+      <div>
+        <Typography style={{ paddingLeft: 30, paddingTop: 50 }} variant="h5">
+          즐겨찾기
+        </Typography>
+        <div
+          style={{
+            paddingLeft: 30,
+            paddingRight: 30
+          }}
+        >
+          <div>
+            <List>
+              {this.state.favorites &&
+                this.state.favorites.map(p => {
+                  let name;
+                  if (p.sname === "") {
+                    name = p.hname;
+                  } else if (p.hname === "") {
+                    name = p.sname;
+                  }
+                  return (
+                    <ListItem key={name}>
+                      <ListItemText primary={name} />
+                      <ListItemSecondaryAction>
+                        <IconButton
+                          onClick={this.handleDeleteButton.bind(this, { p })}
+                          edge="end"
+                          aria-label="delete"
+                        >
+                          <DeleteIcon />
+                        </IconButton>
+                      </ListItemSecondaryAction>
+                    </ListItem>
+                  );
+                })}
+            </List>
+          </div>
+          <Dialog
+            open={this.state.isPopupShow}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">정말로 삭제하시겠습니까?</DialogTitle>
+
+            <DialogActions>
+              <Button text="취소하기" handleButton={this.handleExitButton} />
+              <Button text="삭제하기" handleButton={this.handleDeleteComplete} />
+            </DialogActions>
+          </Dialog>
+        </div>
+      </div>
+    );
+  }
+}
+export default connect(
+  state => ({
+    loggedInfo: state.user.get("loggedInfo"),
+    logged: state.user.get("logged")
+  }),
+  dispatch => ({
+    UserActions: bindActionCreators(userActions, dispatch)
+  })
+)(FavoriteList);
