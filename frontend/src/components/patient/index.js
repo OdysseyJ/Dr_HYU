ce954f1ffdf0065ec9a2c9b7d740a86a5cf55c4a
SeongWoon <odysseyj@naver.com>
2019-12-09 19:44:15 +0900

    path

--- frontend/src/components/patient/index.js
+++
@@ -1,5 +0,0 @@
-export { default as SearchButton } from './SearchButton'
-export { default as ReservationList } from './ReservationList'
-export { default as CurrentVisitList } from './CurrentVisitList'
-export { default as FavoriteList } from './FavoriteList'
-export { default as PrescriptionList } from './PrescriptionList'
