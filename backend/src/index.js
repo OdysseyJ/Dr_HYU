bc3c15de9ce929764ae46cfc818df05966e5c930
SeongWoon <odysseyj@naver.com>
2019-12-09 12:03:56 +0900

    html added

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ const Router = require('koa-router')
 const dotenv = require('dotenv')
 const { jwtMiddleware } = require('./lib/token')
 const db = require('./models')
+const serve = require('koa-static')
+const send = require('koa-send')
 
 // request body 접근용.
 const bodyParser = require('koa-bodyparser')
@@ -20,6 +22,14 @@ router.use('/api', api.routes()) // api 라우트를 /api 경로 하위 라우�
 
 // request를 쉽게쓰기 위한 작업.
 app.use(bodyParser())
+
+// 정적 파일
+app.use(serve(__dirname + '/../' + '/build'))
+app.use(async ctx => {
+  if (ctx.status === 404) {
+    await send(ctx, 'index.html', { root: __dirname + '/../' + '/build' })
+  }
+})
 
 // for jwt
 app.use(jwtMiddleware)
